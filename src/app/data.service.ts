c4081526360b106e58cf39ac69c025b7a689d259
pranay.mhaiske <pranay.mhaiske@kongsbergdigital.com>
2024-02-05 11:47:52 +0530

    third commit

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -6,11 +6,9 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://your-fastapi-backend-api-url';
+  private apiUrl = 'http://127.0.0.1:8000/';
 
   constructor(private http: HttpClient) {}
 
-  sendData(data: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/endpoint`, data);
-  }
+ 
 }
--- src/app/json/json.component.ts
+++ src/app/json/json.component.ts
@@ -1,12 +1,67 @@
 import { Component } from '@angular/core';
-import { HomeComponent } from '../home/home.component';
+
+import { HttpClient } from '@angular/common/http';
+import { async } from 'rxjs/internal/scheduler/async';
+
+interface result {  
+  both: Number;  
+  name: String;  
+  email: String;  
+  gender: String;  
+}  
 @Component({
   selector: 'app-json',
   templateUrl: './json.component.html',
   styleUrls: ['./json.component.css']
 })
+
 export class JsonComponent {
-   
-  
-   
-}
+  enteredText: string = '';
+  enteredText1: string = '';
+  data: any = '';
+  originalJson: any = '';
+  transformedJson: any = '';
+  firstFieldColor = 'green';
+  otherFieldColor = 'yellow';
+  constructor(private httpClient: HttpClient) {}
+
+  async onClickTest() {
+    console.log('Stored Derived Property:', this.enteredText1.split(","));
+    console.log('Stored Attributes:', this.enteredText.split(","));
+
+    const test = {
+      "fields": this.enteredText1.split(","),
+      "attributes": this.enteredText.split(",")
+    };
+
+    await this.httpClient.post('http://127.0.0.1:8000/', test).subscribe(
+      (response) => {
+        this.data = response;
+        console.log('data', this.data);
+
+        // Transform data and log transformedJson
+        this.transformedJson = Object.keys(this.data).map((key) => ({
+          id: key,
+          predicted: this.flattenAndFilterUndefined([
+            this.data[key].both,
+            this.data[key].edit[0],
+            this.data[key].model[0],
+            this.data[key]["model&edit"][0],]),
+        }));
+        console.log('Transformed JSON:', this.transformedJson);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  private flattenAndFilterUndefined(arrays: (any[] | undefined)[]): any[] {
+    return arrays.reduce<any[]>((acc, current) => acc.concat(current || []), []).filter((value) => value !== undefined);
+  }
+
+  handleClick(field: string) {
+    console.log('Clicked:', field);
+    // Add more logic here based on your requirements
+  }
+  }
+
